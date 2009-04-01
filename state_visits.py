d9cdd07b2754fc0f5aed520e3fe1778e2b1d9fa1
Michael Stephens <mike@mindleak.com>
2009-04-01 02:33:51 -0500

    consistent date format

--- state_visits.py
+++ state_visits.py
@@ -2,6 +2,7 @@
 import re
 import csv
 import urllib2
+import datetime as dt
 from BeautifulSoup import BeautifulSoup
 
 def clean(str):
@@ -25,6 +26,48 @@ def clean(str):
 
     return str
 
+def parse_date(date):
+    months = ["January","February","March","April","May","June","July",
+              "August","September","October","November","December"]
+    month_re = '|'.join(months)
+
+    date = date.replace('206', '2006')
+
+    match = re.match('(%s)\s+(\d{1,2})[,.]{0,1}\s+(\d{4})' % month_re, date)
+    if match:
+        month = months.index(match.group(1)) + 1
+        return dt.date(int(match.group(3)),
+                       month,
+                       int(match.group(2))).isoformat()
+
+    match = re.match('(%s)\s+(\d{1,2})-\s{0,}(\d{1,2})[,.]{0,1}\s+(\d{4})' % month_re, date)
+    if match:
+        month = months.index(match.group(1)) + 1
+        d1 = dt.date(int(match.group(4)), month, int(match.group(2)))
+        d2 = dt.date(int(match.group(4)), month, int(match.group(3)))
+        return "%s to %s" % (d1.isoformat(), d2.isoformat())
+
+    match = re.match("(%s)\s+(\d{1,2})-\s{0,}(%s)\s+(\d{1,2})[,.]{0,1}\s+(\d{4})" % (month_re, month_re), date)
+    if match:
+        m1 = months.index(match.group(1)) + 1
+        m2 = months.index(match.group(3)) + 1
+        d1 = dt.date(int(match.group(5)), m1, int(match.group(2)))
+        d2 = dt.date(int(match.group(5)), m2, int(match.group(4)))
+        return "%s to %s" % (d1.isoformat(), d2.isoformat())
+
+    match = re.match("(%s)\s+(\d{1,2})-\s{0,}(\d{1,2}) and (%s)\s+(\d{1,2})-\s{0,}(\d{1,2})[,.]{0,1}\s+(\d{4})" % (month_re, month_re), date)
+    if match:
+        m1 = months.index(match.group(1)) + 1
+        m2 = months.index(match.group(4)) + 1
+        year = int(match.group(7))
+        d1 = dt.date(year, m1, int(match.group(2)))
+        d2 = dt.date(year, m1, int(match.group(3)))
+        d3 = dt.date(year, m2, int(match.group(5)))
+        d4 = dt.date(year, m2, int(match.group(6)))
+        return "%s to %s and %s to %s" % (d1.isoformat(), d2.isoformat(),
+                                   d3.isoformat(), d4.isoformat())
+    return None
+
 def scrape():
     """Grab US State Department lists of visits to the US by foreign
     heads of state and dump them to a CSV file."""
@@ -45,7 +88,9 @@ def scrape():
             cols = row.findAll('td')
             if cols and len(cols) == 4:
                 non_empty = re.compile(".+")
-                date = clean(cols[0].find(text=non_empty))
+                date = parse_date(clean(cols[0].find(text=non_empty)))
+                if not date:
+                    continue
                 visitor = clean(cols[1].find(text=non_empty))
                 country = clean(cols[2].find(text=non_empty))
                 description = clean(cols[3].find(text=non_empty))

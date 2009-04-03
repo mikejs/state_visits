fceea4513261a609a494b96154d2f538487fb419
Michael Stephens <mike@mindleak.com>
2009-04-03 16:39:18 -0400

    grab (most) visits by US President/Secretary of State

--- state_visits.py
+++ state_visits.py
@@ -24,30 +24,32 @@ def clean(str):
     elif str == 'U.S.S.R.':
         str = 'USSR'
 
-    return str
+    return str.encode('ascii', 'replace')
 
 def parse_date(date):
     months = ["January","February","March","April","May","June","July",
               "August","September","October","November","December"]
     month_re = '|'.join(months)
 
-    date = date.replace('206', '2006')
+    date = date.replace('206', '2006').replace('July 68', 'July 8')
+    date = date.replace('October 2023', 'October 20-23')
+    date = date.replace('December 14 and 15', 'December 14-15')
 
-    match = re.match('(%s)\s+(\d{1,2})[,.]{0,1}\s+(\d{4})' % month_re, date)
+    match = re.search('(%s)\s{0,}(\d{1,2})[,.;]{0,1}\s+(\d{4})' % month_re, date)
     if match:
         month = months.index(match.group(1)) + 1
         return dt.date(int(match.group(3)),
                        month,
                        int(match.group(2))).isoformat()
 
-    match = re.match('(%s)\s+(\d{1,2})-\s{0,}(\d{1,2})[,.]{0,1}\s+(\d{4})' % month_re, date)
+    match = re.search('(%s)\s{0,}(\d{1,2})-\s{0,}(\d{1,2})[,.;]{0,1}\s+(\d{4})' % month_re, date)
     if match:
         month = months.index(match.group(1)) + 1
         d1 = dt.date(int(match.group(4)), month, int(match.group(2)))
         d2 = dt.date(int(match.group(4)), month, int(match.group(3)))
         return "%s to %s" % (d1.isoformat(), d2.isoformat())
 
-    match = re.match("(%s)\s+(\d{1,2})-\s{0,}(%s)\s+(\d{1,2})[,.]{0,1}\s+(\d{4})" % (month_re, month_re), date)
+    match = re.search("(%s)\s{0,}(\d{1,2})-\s{0,}(%s)\s+(\d{1,2})[,.;]{0,1}\s+(\d{4})" % (month_re, month_re), date)
     if match:
         m1 = months.index(match.group(1)) + 1
         m2 = months.index(match.group(3)) + 1
@@ -55,7 +57,7 @@ def parse_date(date):
         d2 = dt.date(int(match.group(5)), m2, int(match.group(4)))
         return "%s to %s" % (d1.isoformat(), d2.isoformat())
 
-    match = re.match("(%s)\s+(\d{1,2})-\s{0,}(\d{1,2}) and (%s)\s+(\d{1,2})-\s{0,}(\d{1,2})[,.]{0,1}\s+(\d{4})" % (month_re, month_re), date)
+    match = re.search("(%s)\s{0,}(\d{1,2})-\s{0,}(\d{1,2}) and (%s)\s+(\d{1,2})-\s{0,}(\d{1,2})[,.;]{0,1}\s+(\d{4})" % (month_re, month_re), date)
     if match:
         m1 = months.index(match.group(1)) + 1
         m2 = months.index(match.group(4)) + 1
@@ -68,10 +70,10 @@ def parse_date(date):
                                    d3.isoformat(), d4.isoformat())
     return None
 
-def scrape():
+def in_visits(filename):
     """Grab US State Department lists of visits to the US by foreign
     heads of state and dump them to a CSV file."""
-    out = csv.writer(open('./visits.csv', 'w'))
+    out = csv.writer(open(filename, 'w'))
     out.writerow(['date', 'country', 'visitor', 'description'])
     base_url = "http://www.state.gov/r/pa/ho/c1792.htm"
     base_site = BeautifulSoup(urllib2.urlopen(base_url).read())
@@ -90,11 +92,70 @@ def scrape():
                 non_empty = re.compile(".+")
                 date = parse_date(clean(cols[0].find(text=non_empty)))
                 if not date:
+                    print "Skipping: %s" % str(row)
                     continue
                 visitor = clean(cols[1].find(text=non_empty))
                 country = clean(cols[2].find(text=non_empty))
                 description = clean(cols[3].find(text=non_empty))
                 out.writerow([date, country, visitor, description])
 
+def get_text(elem):
+    str = ""
+    for text in elem.findAll(text=True):
+        str += text
+    return str
+
+def out_visits(url, filename):
+    """Grab US State Department lists of visits by the President and the
+    Secretary of State to foreign countries."""
+    out = csv.writer(open(filename, 'w'))
+    out.writerow(['date', 'country', 'city', 'visitor', 'description'])
+
+    list = BeautifulSoup(urllib2.urlopen(url).read())
+
+    for link in list.find('div', id='body-row02').findAll('a'):
+        visitor = link.string.strip()
+
+        visit_list = BeautifulSoup(urllib2.urlopen(link['href']).read())
+
+        if visitor in ['Condoleezza Rice', 'George W. Bush']:
+            # New style
+            for visit in visit_list.findAll('tr'):
+                if not (visit.td and visit.td.p and visit.td.p.string):
+                    continue
+                date = parse_date(clean(visit.td.p.string))
+                if not date:
+                    continue
+
+                country = clean(get_text(visit.findAll('td')[1].p))
+                city = clean(get_text(visit.findAll('td')[2].p))
+                description = clean(get_text(visit.findAll('td')[3].p))
+                out.writerow([date, country, city, visitor, description])
+        else:
+            for visit in visit_list.find('div', id='centerblock').findAll('p'):
+                if len(visit.contents) < 5:
+                    continue
+
+                date = parse_date(clean(visit.find(text=True)))
+                if not date:
+                    print "Skipping: %s" % str(visit)
+                    continue
+
+                country = clean(visit.findAll(text=True)[1])
+
+                if len(visit.contents) == 7:
+                    city = clean(visit.findAll(text=True)[2])
+                    description = clean(visit.findAll(text=True)[3])
+                else:
+                    city = ""
+                    description = clean(visit.findAll(text=True)[2])
+
+                #print date
+                out.writerow([date, country, city, visitor, description])
+
 if __name__ == "__main__":
-    scrape()
+    in_visits("incoming_visits.csv")
+    out_visits("http://www.state.gov/r/pa/ho/c1792.htm",
+               "./presidential_visits.csv")
+    out_visits("http://www.state.gov/r/pa/ho/trvl/ls/index.htm",
+               "./secretary_visits.csv")
